0f6060ebd6d0eb96af34b5d8c80b77d36222df7e
Hrvoje Pavlinovic <hrvoje.pavlinovic@poviolabs.com>
2022-11-21 15:34:29 +0100

    minor changes

--- src/lambdas/getCurrentDateTime.ts
+++ src/lambdas/getCurrentDateTime.ts
@@ -3,7 +3,10 @@ export default function getCurrentDateTime(): string {
   // const currentTime = new Date(global.Date.now()).toISOString().replace(/T/, " ").replace(/\..+/, "");
 
   // eslint-disable-next-line no-var
-  var currentTime = new Date(global.Date.now()).toISOString().replace(/T/, " ").replace(/\..+/, "");
+  var currentTime = new Date(global.Date.now())
+    .toISOString()
+    .replace(/T/, " ")
+    .replace(/\..+/, "");
   
   return currentTime;
 }
