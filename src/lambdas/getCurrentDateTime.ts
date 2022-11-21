d93ee4e831746ba905a7249cff552f2b0c5fa312
Hrvoje Pavlinovic <hrvoje.pavlinovic@poviolabs.com>
2022-11-21 14:44:48 +0100

    added jest, updated sonar and pipeline

---
+++ src/lambdas/getCurrentDateTime.ts
@@ -0,0 +1,9 @@
+export default function getCurrentDateTime(): string {
+  // useless commented code
+  // const currentTime = new Date(global.Date.now()).toISOString().replace(/T/, " ").replace(/\..+/, "");
+
+  // eslint-disable-next-line no-var
+  var currentTime = new Date(global.Date.now()).toISOString().replace(/T/, " ").replace(/\..+/, "");
+  
+  return currentTime;
+}
